0cd747b2931afe4856029c16649ef9314fcc0697
Juampi <juampi.passi@gmail.com>
2022-04-30 09:54:58 -0300

    Primer commit

---
+++ src/components/Header.vue
@@ -0,0 +1,85 @@
+<template>
+    <div>
+        <v-navigation-drawer
+         
+            v-model="drawer"
+            :clipped="$vuetify.breakpoint.lgAndUp"
+            fixed
+            app
+        >
+
+        <template v-slot:prepend>
+            <v-list-item class="px-1">
+
+               <v-img 
+               src="../assets/logo.jpg"
+               max-height="80px"
+               max-width="240px"
+               contain
+               ></v-img>
+ 
+            </v-list-item>
+        </template>
+
+        <v-divider></v-divider>
+
+        <v-list dense>
+
+            <template>
+            <v-list-item link @click="IrPrincipal">
+                <v-list-item-action>
+                <v-icon color="#ef6b01" >mdi-home</v-icon>
+                </v-list-item-action>
+
+                <v-list-item-content>
+                <v-list-item-title style="color: #424242">Home</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+
+            </template>
+        </v-list>
+
+        </v-navigation-drawer>
+
+        <v-app-bar
+        :clipped-left="$vuetify.breakpoint.lgAndUp"
+        color="#ef6b01"
+        dark
+        app
+        fixed
+        >
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-img
+            class="mx-2"
+            src="../assets/ferrenet.png"
+            max-height="40"
+            max-width="200px"
+            contain
+        ></v-img>
+        <v-spacer></v-spacer>
+
+
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            drawer: null,
+        }
+    },
+    methods: {
+        IrPrincipal(){
+            if (this.$route.path !== '/') 
+            this.$router.push({name: 'Home'});
+        },
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <v-container>
+      <StreamBarcodeReader @decode="(a, b, c) => onDecode(a, b, c)" @loaded="() =>onLoaded()"></StreamBarcodeReader>
+      Input Value: {{ text || "Nothing" }}
+    </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+import { StreamBarcodeReader } from "vue-barcode-reader";
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    StreamBarcodeReader,
+  },
+  data(){
+        return{
+          text: "",
+          id: null, 
+        }
+    },
+    methods: {
+       onDecode(a, b, c) {
+      console.log(a, b, c);
+      this.text = a;
+      if (this.id) clearTimeout(this.id);
+      this.id = setTimeout(() => {
+        if (this.text === a) {
+          this.text = "";
+        }
+      }, 5000);
+    },
+    onLoaded() {
+      console.log("load");
+    },
+    }
 }
 </script>
