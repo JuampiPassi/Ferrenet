2f93ff012ab290346ebf9886c9bb41061416bc87
Juampi <juampi.passi@gmail.com>
2022-04-30 10:35:30 -0300

    gh-pages commit

---
+++ dist/precache-manifest.8b76c1caad27c812d4fc8cf2a9fcdbdd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818fb23b023e6412d4f1",
+    "url": "/ferrenet/css/chunk-vendors.7407d70b.css"
+  },
+  {
+    "revision": "66f34de4e555ee51aa4985638b9700c7",
+    "url": "/ferrenet/img/ferrenet.66f34de4.png"
+  },
+  {
+    "revision": "133eb47cfebaceda609290e78b454c40",
+    "url": "/ferrenet/img/logo.133eb47c.jpg"
+  },
+  {
+    "revision": "acdd7e8255561d6a9c4c5c7a39330982",
+    "url": "/ferrenet/index.html"
+  },
+  {
+    "revision": "e2d97f59d0af894f9ca2",
+    "url": "/ferrenet/js/app.67832601.js"
+  },
+  {
+    "revision": "818fb23b023e6412d4f1",
+    "url": "/ferrenet/js/chunk-vendors.954da291.js"
+  },
+  {
+    "revision": "3d33d7747bc5f1f814c2ac7e5bb95bbb",
+    "url": "/ferrenet/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ferrenet/robots.txt"
+  }
+]);
